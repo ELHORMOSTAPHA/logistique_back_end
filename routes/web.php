7dca73270e9dc69fdaa95fe2bcd837c85edc13e5
mostapha <mostapha.dev.elhor@gmail.com>
2026-04-10 16:28:38 +0100

    text commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    //welcome api response
+    return json_encode([
+        'message' => 'Welcome to MoLogistic API',
+        'version' => '1.0.0',
+        'status' => 'success',
+        'data' => [
+            'name' => 'm-automotiv API',
+            'description' => 'm-automotiv API is a API for managing your m-automotiv needs',
+            'version' => '1.0.0',
+        ]
+    ]);
 });
