a571a261f097ca2a7d942a9a1f663a3c0d2886f9
mostapha <mostapha.dev.elhor@gmail.com>
2026-04-27 22:31:20 +0100

    add message frensh request

--- app/Http/Requests/Stock/StoreStockRequest.php
+++ app/Http/Requests/Stock/StoreStockRequest.php
@@ -29,4 +29,41 @@ class StoreStockRequest extends FormRequest
             'lot_id' => ['required', 'integer', 'exists:lots,id'],
         ];
     }
+
+    /**
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'modele' => 'modèle',
+            'version' => 'version',
+            'vin' => 'numéro de châssis (VIN)',
+            'color_ex' => 'couleur extérieure',
+            'color_ex_code' => 'code couleur extérieure',
+            'color_int' => 'couleur intérieure',
+            'color_int_code' => 'code couleur intérieure',
+            'reserved' => 'réservé',
+            'depot_id' => 'dépôt',
+            'lot_id' => 'lot',
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'Le champ :attribute est obligatoire.',
+            'string' => 'Le champ :attribute doit être une chaîne de caractères.',
+            'max.string' => 'Le champ :attribute ne doit pas dépasser :max caractères.',
+            'boolean' => 'Le champ :attribute doit être vrai ou faux.',
+            'integer' => 'Le champ :attribute doit être un nombre entier.',
+            'exists' => 'La valeur sélectionnée pour :attribute est invalide.',
+            'vin.unique' => 'Ce numéro de châssis (VIN) est déjà attribué à un autre véhicule.',
+            'depot_id.exists' => 'Le dépôt sélectionné est invalide ou n’existe plus.',
+            'lot_id.exists' => 'Le lot sélectionné est invalide.',
+        ];
+    }
 }
--- app/Http/Requests/Stock/UpdateStockRequest.php
+++ app/Http/Requests/Stock/UpdateStockRequest.php
@@ -50,4 +50,61 @@ class UpdateStockRequest extends FormRequest
             'combinaison_rare' => ['sometimes', 'boolean'],
         ];
     }
+
+    /**
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'marque' => 'marque',
+            'numero_commande' => 'numéro de commande',
+            'modele' => 'modèle',
+            'version' => 'version',
+            'finition' => 'finition',
+            'vin' => 'numéro de châssis (VIN)',
+            'color_ex' => 'couleur extérieure',
+            'color_ex_code' => 'code couleur extérieure',
+            'color_int' => 'couleur intérieure',
+            'color_int_code' => 'code couleur intérieure',
+            'client' => 'client',
+            'type_client' => 'type de client',
+            'PGEO' => 'PGEO',
+            'options' => 'options',
+            'vendeur' => 'vendeur',
+            'site_affecte' => 'site affecté',
+            'date_creation_commande' => 'date de création de la commande',
+            'date_arrivage_prevu' => 'date d’arrivage prévue',
+            'date_arrivage_reelle' => 'date d’arrivage réelle',
+            'date_affectation' => 'date d’affectation',
+            'entree_stock_date' => 'date d’entrée en stock',
+            'depot_id' => 'dépôt',
+            'stock_status_id' => 'statut stock',
+            'statut' => 'statut livraison',
+            'numero_lot' => 'numéro de lot',
+            'numero_arrivage' => 'numéro d’arrivage',
+            'lot_id' => 'lot',
+            'combinaison_rare' => 'combinaison rare',
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'Le champ :attribute est obligatoire.',
+            'string' => 'Le champ :attribute doit être une chaîne de caractères.',
+            'max.string' => 'Le champ :attribute ne doit pas dépasser :max caractères.',
+            'date' => 'Le champ :attribute doit être une date valide.',
+            'boolean' => 'Le champ :attribute doit être vrai ou faux.',
+            'integer' => 'Le champ :attribute doit être un nombre entier.',
+            'exists' => 'La valeur sélectionnée pour :attribute est invalide.',
+            'vin.unique' => 'Ce numéro de châssis (VIN) est déjà attribué à un autre véhicule.',
+            'depot_id.exists' => 'Le dépôt sélectionné est invalide ou n’existe plus.',
+            'stock_status_id.exists' => 'Le statut stock sélectionné est invalide.',
+            'lot_id.exists' => 'Le lot sélectionné est invalide.',
+        ];
+    }
 }
